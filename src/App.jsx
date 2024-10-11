3cbb3d8738f6c004f69e4c850d4ce290f01c5f05
VaibhavSutar <sutarvaibhav37@gmail.com>
2024-10-11 11:26:22 -0600

    Added Calendly

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,18 @@ import Navbar from "./components/navbar";
 import HeroSection from "./components/HeroSection";
 import LeftSideBar from "./components/LeftSideBar";
 import ClassicLakeview from "./components/ClassicLakeView";
+import { PopupWidget } from "react-calendly";
 
 function App() {
   return (
     <div className="relative">
+      <PopupWidget
+        url="https://calendly.com/sutarvaibhav37/30min"
+        rootElement={document.getElementById("root")}
+        text="Schedule a meet with us"
+        textColor="#ffffff"
+        color="#08669d"
+      />
       <Navbar />
       <HeroSection />
       <ClassicLakeview/>
