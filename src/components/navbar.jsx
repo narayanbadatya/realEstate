6f76c64974b7d1f5f8509057d3695c66e01d2742
Suyash Labde <suyashlabde123@gmail.com>
2024-10-12 19:46:17 +0530

    minor changes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,8 +4,10 @@ import logo from "../assets/logo.png";
 
 const Navbar = () => {
   return (
-    <nav className="animate-fadeIn sticky top-0 left-0 w-full p-6 flex justify-between text-white z-20">
-      <div className="text-2xl font-bold"><img src={logo} alt="Logo" /></div>
+    <nav className="fixed top-0 left-0 w-full p-6 flex justify-between text-white z-50 bg-transparent">
+      <div className="text-2xl font-bold">
+        <img src={logo} alt="Logo" />
+      </div>
       <ul className="hidden md:flex space-x-6 text-lg">
         <li>
           <Link to="home" smooth={true} duration={500} className="hover:text-gray-300 cursor-pointer">
